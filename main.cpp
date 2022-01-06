0c1396e936a327d75b47f292a0637f7b729996b6
Madeleine Woodbury <mail@woodburycode.com>
2022-01-06 19:11:41 +0100

    get weight and calculate bmi

--- main.cpp
+++ main.cpp
@@ -11,10 +11,28 @@ double get_height()
     return height * 0.3048;
 }
 
+double get_weight()
+{
+    double weight;
+    std::cout << "Enter weight in pounds: ";
+    std::cin >> weight;
+
+    // return weight in kg
+    return weight / 2.2;
+}
+
+double get_bmi()
+{
+    double h = get_height();
+    double w = get_weight();
+
+    double bmi = (w / (h * h));
+    return round(bmi * 100) / 100; // round to 2 decimal places
+}
+
 int main()
 {
     std::cout << "BMI Calculator\n";
-    double height = get_height();
-    height = round(height * 100) / 100; // round to 2 decimal places
-    std::cout << "Your height is " << height << " meters\n";
+    double bmi = get_bmi();
+    std::cout << "Your bmi is " << bmi << std::endl;
 }
