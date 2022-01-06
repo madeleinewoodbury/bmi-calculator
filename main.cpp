c9a5ae626fb63fd91cd7a7ca3d07f4f5efe6e6b2
Madeleine Woodbury <mail@woodburycode.com>
2022-01-06 19:05:36 +0100

    get height function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
+
+double get_height()
+{
+    double height;
+    std::cout << "Enter height in feet: ";
+    std::cin >> height;
+
+    // return height in meters
+    return height * 0.3048;
+}
 
 int main()
 {
     std::cout << "BMI Calculator\n";
+    double height = get_height();
+    height = round(height * 100) / 100; // round to 2 decimal places
+    std::cout << "Your height is " << height << " meters\n";
 }
